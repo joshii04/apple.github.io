ddff134c8f6a9d1ae4fd7a6644b5bbfc833531ec
joshii04 <joshua.sinclair04@gmail.com>
2023-06-07 20:45:24 +0100

    save
    save

--- js/animations.js
+++ js/animations.js
@@ -1,14 +1,15 @@
-const zoom = document.querySelector('.zoom');
-const minZoom = 1;
-const maxZoom = 2;
+// const zoom = document.querySelector('.zoom');
+// const minZoom = 1;
+// const maxZoom = 2;
 
-addEventListener('scroll', e => {
-	const vh = window.innerHeight / 100;
-  const scrollTop = document.documentElement.scrollTop;
-  const start = 100 * vh;
-  const stop = 400 * vh;
-  if (scrollTop > start && scrollTop < stop) {
-	  const scale = Math.max(1.8 - (scrollTop - start) / 500, 1);
-  	zoom.style.transform = `scale(${scale})`
-  }
-})
+// addEventListener('scroll', e => {
+// 	const vh = window.innerHeight / 100;
+//   const scrollTop = document.documentElement.scrollTop;
+//   const start = 100 * vh;
+//   const stop = 400 * vh;
+//   if (scrollTop > start && scrollTop < stop) {
+// 	  const scale = Math.max(1.8 - (scrollTop - start) / 500, 1);
+//   	zoom.style.transform = `scale(${scale})`
+//   }
+// })
+
